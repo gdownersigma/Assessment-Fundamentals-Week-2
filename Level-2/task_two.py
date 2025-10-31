d16b57abffc3d06ddd6eb00a3bef67d483d52a58
George <trainee.george.downer@sigmalabs.co.uk>
2025-10-31 14:20:57 +0000

    First changes to level-2

--- Level-2/task_two.py
+++ Level-2/task_two.py
@@ -1,18 +1,50 @@
 from datetime import date
 
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 1 BELOW
-#
-#####
+class Assessment:
+    """Blueprint for an Assessment class."""
+
+    def __init__(self, name: str, type: str, score: float):
+        """Constructor for Assessment class."""
+        # validators
+        if type not in ["multiple-choice", "technical", "presentation"]:
+            raise ValueError(
+                "Type must be 'multiple-choice', 'technical' or 'presentation'.")
+        if not 0 <= score <= 100:
+            raise ValueError("Score must be in range 0-100.")
+
+        self.name = name
+        self.type = type
+        self.score = score
 
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 1 ABOVE
-#
-#####
+class Trainee:
+    """Blueprint for the Trainee class."""
+
+    def __init__(self, name: str, email: str, date_of_birth: date,):
+        """Constructor for Trainee class."""
+        self.name = name
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.assessments = []
+
+    def get_age(self) -> int:
+        """Method to return the age of this trainee."""
+        return date.today().year - self.date_of_birth.year - \
+            ((date.today().month, date.today().day) <
+             (self.date_of_birth.month, self.date_of_birth.day))
+
+    def add_assessment(self, assessment: Assessment) -> None:
+        """Method to add an assessment to a trainees list of assessments."""
+        self.assessments.append(assessment)
+
+    def get_assessment(self, name: str):
+        """Method to return an assessment if found, if not return None."""
+        for assessment in self.assessments:
+            if assessment.name == name:
+                return assessment
+
+        return None
 
 
 if __name__ == "__main__":
