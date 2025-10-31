f2940a9264c0ea45c675bac0ecab5fe8ae7d2e29
George <trainee.george.downer@sigmalabs.co.uk>
2025-10-31 15:01:27 +0000

    First commit of level 3

--- Level-3/task_three.py
+++ Level-3/task_three.py
@@ -1,17 +1,115 @@
-
-#####
-#
-# COPY YOUR CODE FROM LEVEL 2 BELOW
-#
-#####
+"""Level 3 of the assessment."""
+from datetime import date
 
 
+class Assessment:
+    """Blueprint for an Assessment class."""
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 2 ABOVE
-#
-#####
+    def __init__(self, name: str, score: float):
+        """Constructor for Assessment class."""
+        # validators
+        if not 0 <= score <= 100:
+            raise ValueError("Score must be in range 0-100.")
+
+        self.name = name
+        self.score = score
+        self.weight = 0
+
+    def calculate_score(self):
+        """Method to return the calculated score."""
+        return self.score * self.weight
+
+
+class Trainee:
+    """Blueprint for the Trainee class."""
+
+    def __init__(self, name: str, email: str, date_of_birth: date,):
+        """Constructor for Trainee class."""
+        self.name = name
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.assessments = []
+
+    def get_age(self) -> int:
+        """Method to return the age of this trainee."""
+        return date.today().year - self.date_of_birth.year - \
+            ((date.today().month, date.today().day) <
+             (self.date_of_birth.month, self.date_of_birth.day))
+
+    def add_assessment(self, assessment: Assessment) -> None:
+        """Method to add an assessment to a trainees list of assessments."""
+        if not isinstance(assessment, Assessment):
+            raise TypeError(
+                "An assessment should be a subclass of Assessment.")
+        self.assessments.append(assessment)
+
+    def get_assessment(self, name: str):
+        """Method to return an assessment if found, if not return None."""
+        for assessment in self.assessments:
+            if assessment.name == name:
+                return assessment
+
+        return None
+
+    def get_assessment_of_type(self, type: str) -> list[Assessment]:
+        """Method to return all assessments of a given type the trainee has."""
+
+        if type.lower() not in ["multiple-choice", "technical", "presentation"]:
+            raise ValueError(
+                "Type must be 'multiple-choice', 'technical' or 'presentation'.")
+
+        assessments_of_type = []
+        for assessment in self.assessments:
+            print(type)
+            print(assessment.__str__())
+            if type in assessment.__str__():
+                assessments_of_type.append(assessment)
+        return assessments_of_type
+
+
+class MultipleChoiceAssessment(Assessment):
+    """Blueprint for a multiple choice type of assessment."""
+
+    def __init__(self, name: str, score: float):
+        """Constructor method for multiple choice assessment."""
+        super().__init__(name, score)
+        self.weight = 0.7
+
+    def __str__(self):
+        return f"<multiple-choice Assessment: {self.name}>"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class TechnicalAssessment(Assessment):
+    """Blueprint for a technical type of assessment."""
+
+    def __init__(self, name: str, score: float):
+        """Constructor method for technical assessment."""
+        super().__init__(name, score)
+        self.weight = 1
+
+    def __str__(self):
+        return f"<technical Assessment: {self.name}>"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class PresentationAssessment(Assessment):
+    """Blueprint for a presentation type of assessment."""
+
+    def __init__(self, name: str, score: float):
+        """Constructor method for presentation assessment."""
+        super().__init__(name, score)
+        self.weight = 0.6
+
+    def __str__(self):
+        return f"<presentation Assessment: {self.name}>"
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class Question:
